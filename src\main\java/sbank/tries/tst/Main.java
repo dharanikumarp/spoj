375b05d03bb1dccfe0b5ebfe8285edab11923988
dharani.kumar@gmail.com <dharanikumarp>
2014-10-23 00:55:44 +0530

    Init file for ternary search trie implementation.

---
+++ src\main\java/sbank/tries/tst/Main.java
@@ -0,0 +1,17 @@
+package sbank.tries.tst;
+
+/**
+ * @author dharanikumar
+ * 
+ * Solution to SBANK problem in SPOJ using ternary search tries.
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+	}
+
+}
