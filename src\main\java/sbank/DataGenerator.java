87fa19792827cdd8a536be75bf3555128ecf2e7c
dharani.kumar@gmail.com <dharanikumarp>
2014-10-27 23:53:10 +0530

    Adding java documentation comments for the random account number generating functions.

--- src\main\java/sbank/DataGenerator.java
+++ src\main\java/sbank/DataGenerator.java
@@ -1,17 +1,15 @@
 package sbank;
 
 import java.util.Random;
-import java.util.Scanner;
 
 /**
- * Generate test data to verify SBANK problem in SPOJ
+ * Generate random test data to verify SBANK problem in SPOJ The algorithm
+ * creates a seed for every test case and generate random strings by altering a
+ * number at random position using the previous seed.
  * 
- * The program will generate random test data once and shuffle the same into set
- * of numTestCases.
  * 
- * Invoke the program as follows to redirect the text to a file.
- * 
- * java DataGenerator <numTestcases> <numRecordsPerTestCase> > <file>
+ * Invoke the program as follows to redirect the text to a file. java
+ * DataGenerator <numTestcases> <numRecordsPerTestCase> > <file>
  * 
  * for example java DataGenerator 5 100000 > testcase1.txt
  * 
@@ -19,18 +17,19 @@ import java.util.Scanner;
  */
 public class DataGenerator {
 	public static final int ACC_NUM = 26;
+
+	// Including the space at the end of the account number
 	public static final int ACC_NUM_FORMATTED = 26 + 7;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int numTestCases = sc.nextInt();
-		int recordsPerTestCase = sc.nextInt();
+		int numTestCases = Integer.valueOf(args[0]);
+		int recordsPerTestCase = Integer.valueOf(args[1]);
 
 		Random randRadix = new Random();
+		Random randPosition = new Random();
 
 		System.out.println(numTestCases);
 
@@ -40,19 +39,30 @@ public class DataGenerator {
 			// line characters
 			StringBuilder sb = new StringBuilder((numTestCases + 1)
 					* (ACC_NUM_FORMATTED + 2));
+			String seed = getRandAccNum(randRadix);
 
-			sb.append(numTestCases).append('\n');
+			sb.append(recordsPerTestCase).append('\n');
+			sb.append(seed).append('\n');
 
-			for (int j = 0; j < recordsPerTestCase; j++) {
-				sb.append(generateAccountNumber(randRadix)).append('\n');
+			for (int j = 1; j < recordsPerTestCase; j++) {
+				String accNum = generateAccountNumber(randRadix, randPosition,
+						seed);
+				sb.append(accNum).append('\n');
+				seed = accNum;
 			}
 			System.out.println(sb.toString());
 		}
-
-		sc.close();
 	}
 
-	private static String generateAccountNumber(Random randRadix) {
+	/**
+	 * Generate a random account number by iterating over all possible positions
+	 * and using a Random generator as input.
+	 * 
+	 * @param randRadix
+	 *            random generator for generating radix
+	 * @return String having the formatted account number.
+	 */
+	private static String getRandAccNum(Random randRadix) {
 		StringBuilder sb = new StringBuilder(ACC_NUM_FORMATTED);
 
 		for (int i = 0; i < ACC_NUM_FORMATTED - 1; i++) {
@@ -64,5 +74,47 @@ public class DataGenerator {
 			}
 		}
 		return sb.toString();
+	}
+
+	/**
+	 * Generates a random account number using a seed account number by randomly
+	 * changing a value at a position.
+	 * 
+	 * @param randRadix
+	 *            Random generator used to generate numbers between 0 to 9
+	 * @param randPosition
+	 *            Random generator used to generate positions between 0 to 32
+	 * @param prevAccountNumber
+	 *            Seed used for generating new number
+	 * @return String having the formatted account number.
+	 */
+	private static String generateAccountNumber(Random randRadix,
+			Random randPosition, String prevAccountNumber) {
+
+		String accountString = null;
+
+		while (true) {
+			int i = randPosition.nextInt(ACC_NUM_FORMATTED - 1);
+
+			// Ignore the places where ' ' character is present.
+			if (i == ACC_NUM_FORMATTED - 2 || i == 26 || i == 21 || i == 16
+					|| i == 11 || i == 2) {
+				continue;
+			} else {
+				int number = randRadix.nextInt(10);
+
+				if (number == Integer.valueOf(prevAccountNumber.charAt(i))) {
+					continue;
+				} else {
+					char[] accountNumber = prevAccountNumber.toCharArray();
+					accountNumber[i] = Integer.valueOf(number).toString()
+							.charAt(0);
+					accountString = new String(accountNumber);
+					break;
+				}
+			}
+		}
+
+		return accountString;
 	}
 }
